1dae938ce4d8abf8e8d22b4689974b0bc28d1103
Katrina Roaix <kroaix@gmail.com>
2019-11-24 23:51:36 -0500

    configured migrations up and down

--- data/migrations/20191124204642_create-tables.js
+++ data/migrations/20191124204642_create-tables.js
@@ -1,8 +1,50 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("projects", project => {
+      project.increments();
+      project.string("project_name").notNullable();
+      project.text("project_description");
+      project.boolean("project_completed").defaultTo(false);
+    })
+    .createTable("tasks", task => {
+      task.increments();
+      task.text("task_description").notNullable();
+      task.string("task_notes");
+      task.boolean("task_completed").defaultTo(false);
+      task
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("projects.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("resources", resource => {
+      resource.increments();
+      project.string("resource_name").notNullable();
+      project.text("resource_description");
+    })
+    .createTable("project_resources", pr => {
+      pr.increments();
+      pr.integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("projects.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      pr.integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("resources.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("projects")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("project_resources");
 };
