d7bd5a85944e5345b00605df3356bc34cf33f9bf
Katrina Roaix <kroaix@gmail.com>
2019-11-25 00:21:21 -0500

    Added seed for tasks table

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,16 +1,19 @@
 exports.seed = function(knex) {
   return knex("projects").insert([
     {
+      id: 1,
       project_name: "Finish Sprint",
       project_description: "Complete MVP!",
       project_completed: false
     },
     {
+      id: 2,
       project_name: "Get Some Rest",
       project_description: "Sprint was hard, time to relax.",
       project_completed: false
     },
     {
+      id: 3,
       project_name: "Have a Happy Holiday",
       project_description: "Thanksgiving means food! Yum.",
       project_completed: false
