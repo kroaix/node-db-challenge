60739d344c5dfc3bfaa4589e4a21ad88747adc14
Katrina Roaix <kroaix@gmail.com>
2019-12-02 02:57:28 -0500

    tasks router/model complete.. made some changes

--- tasks/tasks-model.js
+++ tasks/tasks-model.js
@@ -1,3 +1,30 @@
 const db = require("../data/db-config");
 
-module.exports = {};
+function find(){
+  return db("tasks")
+    .select(
+      "tasks.id",
+      "tasks.task_description",
+      "tasks.task_notes",
+      "tasks.task_completed",
+      "projects.project_name",
+      "projects.project_description"
+    )
+    .from("tasks")
+    .join("projects", "projects.id", "tasks.project_id")
+}
+
+function getProject(id){
+  return db('projects')
+  .where({'projects.id': id})
+}
+
+async function addTask(id, task){
+  await getProject(id);
+  return db('tasks').insert(task);
+}
+
+module.exports = {
+  find,
+  addTask
+};
--- tasks/tasks-router.js
+++ tasks/tasks-router.js
@@ -4,4 +4,17 @@ const Tasks = require("./tasks-model");
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Tasks.find();
+    const updatedTasks = [...tasks].map(task => {
+      task.task_completed = Boolean(task.task_completed);
+      return task;
+    });
+    res.status(200).json({ success: true, updatedTasks });
+  } catch {
+    res.status(500).json({ success: false, error: "Problem with request." })
+  }
+})
+
 module.exports = router;
