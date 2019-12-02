b89e574ebb78a8b5ca7ca14bc752b3010e847b0b
Katrina Roaix <kroaix@gmail.com>
2019-12-02 01:19:17 -0500

    projects router/model complete

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,7 +1,14 @@
 const db = require('../data/db-config');
 
+function find(){
+  return db('projects');
+}
 
+function add(project){
+  return db('projects').insert(project);
+}
 
 module.exports = {
-  
+  find,
+  add
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,6 +4,31 @@ const Projects = require("./projects-model");
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const projects = await Projects.find();
+    const intToBool = [...projects].map(project => {
+      project.project_completed = Boolean(project.project_completed);
+      return project;
+    })
+    res.status(200).json({ success: true, intToBool }); 
+  } catch {
+    res.status(500).json({ success: false, error: "Problem with request." })
+  }
+})
 
+router.post('/', async (req, res) => {
+  const projectData = req.body;
+  try {
+    if(!projectData.project_name){
+      res.status(400).json({ success: false, error: "Name is required." })
+    } else {
+      const project = await Projects.add(projectData);
+      res.status(201).json({ success: true, project });
+    }
+  } catch {
+    res.status(500).json({ success: false, error: "Problem with request." })
+  }
+})
 
 module.exports = router;
